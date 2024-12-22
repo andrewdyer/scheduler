e12c13d018809c1e218a802afd3a99afe4d1a90c
Andrew Dyer <andrewdyer@outlook.com>
2024-12-22 22:23:44 +0000

    docs: :bulb: add PHPDoc block
    Added PHPDoc blocks to AbstractJob methods

--- src/Scheduler.php
+++ src/Scheduler.php
@@ -5,16 +5,32 @@ namespace Anddye\Scheduler;
 use DateTime;
 use DateTimeInterface;
 
+/**
+ * Class Scheduler
+ *
+ * @package Anddye\Scheduler
+ */
 class Scheduler
 {
     private $datetime;
     private $jobs = [];
 
+    /**
+     * Scheduler constructor.
+     *
+     * @param DateTimeInterface|null $datetime
+     */
     public function __construct(DateTimeInterface $datetime = null)
     {
         $this->datetime = $datetime;
     }
 
+    /**
+     * Add a job to the scheduler.
+     *
+     * @param AbstractJob $job
+     * @return AbstractJob
+     */
     public function addJob(AbstractJob $job): AbstractJob
     {
         $this->jobs[] = $job;
@@ -22,6 +38,11 @@ class Scheduler
         return $job;
     }
 
+    /**
+     * Get the current datetime.
+     *
+     * @return DateTimeInterface
+     */
     public function getDatetime(): DateTimeInterface
     {
         if (!$this->datetime) {
@@ -31,11 +52,21 @@ class Scheduler
         return $this->datetime;
     }
 
+    /**
+     * Get the list of jobs.
+     *
+     * @return array
+     */
     public function getJobs(): array
     {
         return $this->jobs;
     }
 
+    /**
+     * Run the scheduler.
+     *
+     * @return void
+     */
     public function run(): void
     {
         foreach ($this->getJobs() as $job) {
