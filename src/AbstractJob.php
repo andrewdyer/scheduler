a1a5b51ebaa1026b1f091c0cce4b155de0a1eae9
Andrew Dyer <andrewdyer@outlook.com>
2024-12-22 22:14:31 +0000

    docs: :bulb: add PHPDoc block
    Added PHPDoc blocks to AbstractJob methods

--- src/AbstractJob.php
+++ src/AbstractJob.php
@@ -5,14 +5,30 @@ namespace Anddye\Scheduler;
 use Cron\CronExpression;
 use DateTimeInterface;
 
+/**
+ * Class AbstractJob
+ *
+ * @package Anddye\Scheduler
+ */
 abstract class AbstractJob
 {
     use Frequencies;
 
     private $expression = '* * * * *';
 
+    /**
+     * Handle the job.
+     *
+     * @return void
+     */
     abstract public function handle(): void;
 
+    /**
+     * Check if the job is due to run.
+     *
+     * @param DateTimeInterface $currentTime
+     * @return bool
+     */
     public function isDueToRun(DateTimeInterface $currentTime): bool
     {
         return (new CronExpression($this->getExpression()))->isDue($currentTime);
